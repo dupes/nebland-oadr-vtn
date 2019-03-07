497c4de6331fbaa9f023d4d36b65affa9fd9a6e7
Benjamin DuPont <ben@nebland.com>
2019-03-07 12:04:25 -0600

    Transfer gh-pages

--- db/migrate/20160923192035_add_name_to_tou_schedules.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToTouSchedules < ActiveRecord::Migration
-  def change
-    add_column :tou_schedules, :name, :string
-  end
-end
--- db/migrate/20160923193100_unique_tou_schedule_name.rb
+++
@@ -1,6 +0,0 @@
-class UniqueTouScheduleName < ActiveRecord::Migration
-  def up
-    add_index :tou_schedules, :name, unique: true
-  end
-
-end
